fc5ef07cc5e7be1cc1532039c7be3d34aaee5fd7
Ko Seoyoon <ge97qus@mytum.de>
2020-06-14 07:55:43 +0800

    resolve conflict

--- React/src/index.js
+++ React/src/index.js
@@ -11,30 +11,17 @@ import Routes from './routes/routes';
 // import CCTV from './cctv';
 
 ReactDOM.render(
-<<<<<<< HEAD
-
-  <div className="App">
-    {/* <Navigation /> */}
-    <Routes />
-  </div>
-  ,
-  document.getElementById('root')
-);
-=======
-    <Router>
-        <div className="App">
-            <Navbar bg="dark" variant="dark" className="bar" sticky="top">
-                <Navbar.Brand href="/" className="brand">The Kampung Network</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                </Nav>
-                <Nav >
-                    <Nav.Link className="mr-auto" href="#pricing">Log Out</Nav.Link>
-                </Nav>
-            </Navbar>
-            <Routes />
-        </div>
-    </Router>,
+    <div className="App">
+        <Navbar bg="dark" variant="dark" className="bar" sticky="top">
+            <Navbar.Brand href="/" className="brand">The Kampung Network</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+            <Nav >
+                <Nav.Link className="mr-auto" href="#pricing">Log Out</Nav.Link>
+            </Nav>
+        </Navbar>
+        <Routes />
+    </div>,
     document.getElementById('root')
 );
->>>>>>> e7ceb64d1de7b02dd9dcd191fb65312fd347975f

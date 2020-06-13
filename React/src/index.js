efcad329cd5f9589056267c3d83508c2065a0728
Ko Seoyoon <ge97qus@mytum.de>
2020-06-13 14:09:10 +0800

    add accident detection + file creation

--- React/src/index.js
+++ React/src/index.js
@@ -6,10 +6,26 @@ import ObjectDetectionVideo from './object-detection-video/ObjectDetectionVideo'
 
 import './index.css'
 
+var accident
+var frame = []
 const handlePrediction = (predictions) => {
-  console.timeEnd('detect')
-  console.time('detect')
+  if(Date.now() - frame[0] >= 1000){
+    if (frame.length > 5) {
+      console.log(accident)
+      alert(accident)
+      frame = []
+    } else frame = []
+  }
+  predictions.forEach((prediction) => {
+    if (prediction.label === 'Peaceful situation'){
+        accident = prediction.label
+        frame.push(Date.now())
+      }
+  })  
   console.log(predictions)
+  // console.timeEnd('detect')
+  // console.time('detect')
+
 }
 
 const render = (ctx, predictions) => {
@@ -37,7 +53,7 @@ const App = () => {
     <div className="fillPage">
       <ObjectDetectionVideo
         model={model}
-        // onPrediction={handlePrediction}
+        onPrediction={handlePrediction}
         // render={render}
         // aspectFill: The option to scale the video to fill the size of the view.
         //             Some portion of the video may be clipped to fill the view's
